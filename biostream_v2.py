7112b1208b69c43601626e4f68279eff6043f9b3
pedrorivera40 <pedro.rivera40@upr.edu>
2020-03-10 20:20:27 -0400

    Adding data collection from headset only.

--- biostream_v2.py
+++ biostream_v2.py
@@ -1,5 +1,4 @@
-
-
+from pylsl import StreamInlet, resolve_stream
 
 class BioStream:
     '''
@@ -29,16 +28,40 @@ class BioStream:
             It stores the 8-channels values along with their corresponding timestamp.
         '''
         def __init__(self, channels, timestamp):
-            self.channels = channels_data
+            self.channels = channels
             self.timestamp = timestamp
 
+    def find_lsl_index(self, stream_name):
+        # Find EEG streams that are using the Lab Streaming Layer.
+        self.streams = resolve_stream('type', 'EEG')
 
+        # Validate if stream_name is connected.
+        stream_index = -1
+        for i in range(0, len(self.streams)):
+            if stream_name == self.streams[i].name():
+                stream_index = i
+        
+        return stream_index
 
     # Defining data collection methods.
 
-    # TODO -> Implement data collection method only considering data from the headset.
+    # TODO -> Test this method...
     def collect_mark4lsl(self, num_samples, lsl_name):
-        return 0
+        
+        stream_index = self.find_lsl_index(lsl_name)
+        if stream_index < 0:
+            raise ValueError("LSL device not connected:", lsl_name)
+
+        # Create a new inlet to read from the stream_name.
+        self.inlet = StreamInlet(self.streams[stream_index])
+        headset_entries = []
+
+        while num_samples > 0:
+            bci_sample, timestamp = self.inlet.pull_sample()
+            headset_entries.append(self.Mark4_Entry(bci_sample, timestamp))
+            num_samples -= 1
+        
+        return headset_entries
 
     # TODO -> Implement data collection method considering data from headset & knee angle.
     def collect_mark4lsl_kneeserial(self, num_samples, lsl_name, serial_name, baud_rate):
@@ -50,7 +73,7 @@ if __name__ == '__main__':
 
     # Prepairing Mock Test.
     num_samples = 20
-    lsl_name = "cool_stream"
+    lsl_name = "BioSemi"
     serial_name = "cool_serial"
     baud_rate = 9600
 
@@ -63,6 +86,8 @@ if __name__ == '__main__':
 
     # Displaying Mark4 results.
     print("Mark4:", data_mark4)
+    for val in data_mark4:
+        print(val.timestamp)
 
     # Displaying Mark4 + Knee Angle results.
     print("Mark4 + Knee Angle:", data_mark4_knee)
