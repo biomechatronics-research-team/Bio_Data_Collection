e75400d7b3ee0f0736b64edfa10f3fa88c1336b0
Maria Marrero <maria.marrero13@upr.edu>
2020-03-27 12:52:17 -0400

    added main method in order to be used in testet

--- lsl_emulator.py
+++ lsl_emulator.py
@@ -5,22 +5,26 @@ import random
 import time
 from pylsl import StreamInfo, StreamOutlet
 
-# Initialize an LSL stream by providing the stream-name ('BioSemi'),
-# signal type ('EEG'), number of channels (8 for the OpenBCI Mark IV headset),
-# sampling frequency (250 for the OpenBCI Mark IV headset), the data type ('float32'),
-# and the user-id.
-info = StreamInfo('BioSemi', 'EEG', 8, 250, 'float32', 'myuid34234')
+def start_lsl():
+    # Initialize an LSL stream by providing the stream-name ('BioSemi'),
+    # signal type ('EEG'), number of channels (8 for the OpenBCI Mark IV headset),
+    # sampling frequency (250 for the OpenBCI Mark IV headset), the data type ('float32'),
+    # and the user-id.
+    info = StreamInfo('BioSemi', 'EEG', 8, 250, 'float32', 'myuid34234')
 
-# Create a server outlet.
-outlet = StreamOutlet(info)
+    # Create a server outlet.
+    outlet = StreamOutlet(info)
 
-while True:
+    while True:
 
-    # Create random 8-channel sample.
-    mysample = [random.random(), random.random(), random.random(),
-                random.random(), random.random(), random.random(),
-                random.random(), random.random()]
+        # Create random 8-channel sample.
+        mysample = [random.random(), random.random(), random.random(),
+                    random.random(), random.random(), random.random(),
+                    random.random(), random.random()]
 
-    # Send the signal and wait 4ms for the next data post (emulating 250 Hz sampling rate).
-    outlet.push_sample(mysample)
-    time.sleep(0.04)
+        # Send the signal and wait 4ms for the next data post (emulating 250 Hz sampling rate).
+        outlet.push_sample(mysample)
+        time.sleep(0.04)
+
+if __name__ == '__main__':
+    start_lsl()
