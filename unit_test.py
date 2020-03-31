2707a6d1646f925a0fd1a3c8d3c2f6182819c038
Maria Marrero <maria.marrero13@upr.edu>
2020-03-30 22:59:18 -0400

    Completed test cases for lsl disconnection

--- unit_test.py
+++ unit_test.py
@@ -2,46 +2,53 @@ import unittest
 from lsl_emulator import start_lsl
 import biostream_v2
 from multiprocessing import Process
+from time import sleep
 
 class TestBioStream(unittest.TestCase):
 
-    # *** Initialization Unit Tests... ***
-    #def setUp(self):
-     lsl_error =  "LSL device not connected:"
-     serial_error = "Serial Port not connected:"
+    lsl_error =  "LSL device not connected:"
+    serial_error = "Serial Port not connected:"
+
+
 
     # *** Run Data Collection Unit Tests... ***
     #tests should start with "test_" in order to be considered
 
     # *** tests for LSL only *** #
     def test_lslnotconnected(self):
-        with self.assertRaisesRegex(ValueError, lsl_error):
-            biostream_v2.main_test()
-         
+        biostream_process = Process(target=  biostream_v2.main_test)
+        biostream_process.start()
+        biostream_process.join()
+        self.assertEqual(biostream_process.exitcode, 1)
+       
     def test_lsldisconnection(self):
         lsl_emulator_process = Process(target=start_lsl)
-        biostream_process = Process(target=  biostream_v2.main_test())
+        biostream_process = Process(target=  biostream_v2.main_test)
         lsl_emulator_process.start()
-        with self.assertRaisesRegex(ValueError, lsl_error):
-            biostream_process.start()
-            time.sleep(100)
-            lsl_emulator_process.terminate()
-            biostream_process.join()
+        biostream_process.start()
+        sleep(1)
+        lsl_emulator_process.terminate()
+        biostream_process.join()
+        self.assertEqual(biostream_process.exitcode, 3)
 
    # def test_numberofsamples(self):
 
     
-    
     # *** tests for LSL and Knee Encoder (serial device) *** #
+    # TODO -> Simulate serial device
     def test_lslnotconnected_2(self):
-        with self.assertRaisesRegex(ValueError, lsl_error):
-            biostream_v2.main_test()
+        biostream_process = Process(target= biostream_v2.main_test)
+        biostream_process.start()
+        biostream_process.join()
+        self.assertEqual(biostream_process.exitcode, 1)
 
     def test_serialnotconnected(self):
         lsl_emulator = Process(target= start_lsl)
         lsl_emulator.start()
-        with self.assertRaisesRegex(ValueError, serial_error):
-            biostream_v2.main_test()
+        biostream_process = Process(target=  biostream_v2.main_test)
+        biostream_process.start()
+        biostream_process.join()
+        self.assertEqual(biostream_process.exitcode, 2)
         lsl_emulator.terminate()
     
     """
