e293056b7bddb53b1d616d9d3d5f9fd8caefdcf6
Maria Marrero <maria.marrero13@upr.edu>
2020-04-01 15:34:12 -0400

    coded tests regarding serial connection

--- unit_test.py
+++ unit_test.py
@@ -1,20 +1,16 @@
 import unittest
 from lsl_emulator import start_lsl
+from serial_emulator import start_serial
 import biostream_v2
 from multiprocessing import Process
 from time import sleep
 
 class TestBioStream(unittest.TestCase):
 
-    lsl_error =  "LSL device not connected:"
-    serial_error = "Serial Port not connected:"
+    """ *** Run Data Collection Unit Tests... 
+    tests should start with "test_" in order to be considered """
 
-
-
-    # *** Run Data Collection Unit Tests... ***
-    #tests should start with "test_" in order to be considered
-
-    # *** tests for LSL only *** #
+    """ *** tests for LSL only *** """
     def test_lslnotconnected(self):
         biostream_process = Process(target=  biostream_v2.main_test)
         biostream_process.start()
@@ -23,7 +19,7 @@ class TestBioStream(unittest.TestCase):
        
     def test_lsldisconnection(self):
         lsl_emulator_process = Process(target=start_lsl)
-        biostream_process = Process(target=  biostream_v2.main_test)
+        biostream_process = Process(target= biostream_v2.main_test)
         lsl_emulator_process.start()
         biostream_process.start()
         sleep(1)
@@ -31,33 +27,57 @@ class TestBioStream(unittest.TestCase):
         biostream_process.join()
         self.assertEqual(biostream_process.exitcode, 3)
 
-   # def test_numberofsamples(self):
+    #def test_numberofsamples(self):
 
     
-    # *** tests for LSL and Knee Encoder (serial device) *** #
-    # TODO -> Simulate serial device
+    """ *** tests for LSL and Knee Encoder (serial device) *** """
     def test_lslnotconnected_2(self):
+        serial_emulator_process = Process(target= start_serial)
         biostream_process = Process(target= biostream_v2.main_test)
+        serial_emulator_process.start()
         biostream_process.start()
         biostream_process.join()
         self.assertEqual(biostream_process.exitcode, 1)
+        serial_emulator_process.terminate()
 
     def test_serialnotconnected(self):
-        lsl_emulator = Process(target= start_lsl)
-        lsl_emulator.start()
+        lsl_emulator_process = Process(target= start_lsl)
+        lsl_emulator_process.start()
         biostream_process = Process(target=  biostream_v2.main_test)
         biostream_process.start()
         biostream_process.join()
         self.assertEqual(biostream_process.exitcode, 2)
-        lsl_emulator.terminate()
+        lsl_emulator_process.terminate()
     
-    """
     def test_lsldisconnection_2(self):
+        lsl_emulator_process = Process(target=start_lsl)
+        serial_emulator_process = Process(target= start_serial)
+        biostream_process = Process(target= biostream_v2.main_test)
+        lsl_emulator_process.start()
+        serial_emulator_process.start()
+        biostream_process.start()
+        sleep(1)
+        lsl_emulator_process.terminate()
+        biostream_process.join()
+        self.assertEqual(biostream_process.exitcode, 3)
+        serial_emulator_process.terminate()
     
     def test_serialdisconnection(self):
+        lsl_emulator_process = Process(target=start_lsl)
+        serial_emulator_process = Process(target= start_serial)
+        biostream_process = Process(target= biostream_v2.main_test)
+        lsl_emulator_process.start()
+        serial_emulator_process.start()
+        biostream_process.start()
+        sleep(1)
+        serial_emulator_process.terminate()
+        biostream_process.join()
+        self.assertEqual(biostream_process.exitcode, 3)
+        lsl_emulator_process.terminate()
+
+
+    #def test_numberofsamples_2(self):
     
-    def test_numberofsamples_2(self):
-    """
 
 if __name__ == '__main__':
    unittest.main()
